85fef6cbc0ca8ff143541dd4f3ee0c9d4b64cd3f
Shiva Mishra <xemishra@gmail.com>
2024-12-20 10:01:33 +0530

    Update and rename core to core.py

---
+++ Pixelroid/core.py
@@ -0,0 +1,13 @@
+from pyrogram import Client
+
+class PixelroidBot:
+    def __init__(self, api_id: int, api_hash: str, bot_token: str):
+        self.api_id = api_id
+        self.api_hash = api_hash
+        self.bot_token = bot_token
+        self.xemishra = Client(
+            name="PornBot",
+            api_id=self.api_id,
+            api_hash=self.api_hash,
+            bot_token=self.bot_token
+        )
